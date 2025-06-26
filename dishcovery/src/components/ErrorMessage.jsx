60e3e1badbb417915c3003954f83c3700be011c6
Aitijhya <raitijhya005@gmail.com>
2025-06-26 21:24:20 +0530

    Create ErrorMessage.jsx

---
+++ dishcovery/src/components/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorMessage = () => {
+  return (
+    <div className='flex justify-center items-center min-h-[200px]'>
+      <h2 className='text-textColor'>No recipes found</h2>
+    </div>
+  )
+}
+
+export default ErrorMessage
