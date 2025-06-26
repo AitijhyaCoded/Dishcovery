992f50bf2b5863f44d9d5a4137ac4b249a5b94a1
Aitijhya <raitijhya005@gmail.com>
2025-06-26 16:24:22 +0530

    intro

--- dishcovery/src/App.jsx
+++ dishcovery/src/App.jsx
@@ -2,5 +2,5 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  return <h1 className="text-4xl text-pink-500 text-center mt-10">Dishcovery 🍽️</h1>;
+  return <h1 className="font-bold text-4xl text-pink-500 text-center mt-10">Dishcovery</h1>;
 }
