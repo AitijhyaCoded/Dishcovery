f01d24b819bf6c3a8687d48af72c4ca04c359f40
Aitijhya <raitijhya005@gmail.com>
2025-06-26 21:24:03 +0530

    Update tailwind.config.js

--- dishcovery/tailwind.config.js
+++ dishcovery/tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        yellowTheme: '#F9DD96',
+        redTheme: '#AC2E2D',
+        textColor: '#1E1E1E',
+        bgColor: '#FFFCF8'
+      },
       fontFamily: {
         playpen: ["'Playpen Sans'", "cursive"],
       },
